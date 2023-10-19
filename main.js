0946afff1d547cf811da0fb37508decf70fb371f
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2023-10-19 18:59:21 +0200

    lang selector

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+const dropBtn = document.querySelector('.dropbtn');
+const dropDownContent = document.querySelector('.dropdown-content');
+dropBtn.addEventListener('click', () => {
+    dropDownContent.style.display = dropDownContent.style.display === 'block' ? 'none' : 'block';
+});
+
+
 const changeLanguage = (language) => {
     document.getElementById("selectedLanguage").innerHTML = `<img src="images/countries/${language}.png" alt="${language}" />`;
+    dropDownContent.style.display = 'none'; // Close the dropdown after selection
 };
-
